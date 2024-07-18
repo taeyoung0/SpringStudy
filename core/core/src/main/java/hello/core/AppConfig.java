c2b230b159c7b0079dfc217a722e03f4c6140c35
taeyoung0 <jty19954@gmail.com>
2024-07-18 18:35:14 +0900

    추가- AppConfig을 역할과 구현 클래스가 한 눈에 들어오게 변경, 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악 가능

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {        // 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다
 
     public MemberService memberService() {      // 생성자 주입, 실제 동작에 필요한 구현 객체를 생성
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성한 객체의 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
+        return new MemberServiceImpl(memberRepository()); // 생성한 객체의 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
     }
 
-    public OrderService orderService() {        // 생성자 주입
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {       // 역할에 따른 구현 클래스를 한 눈에 들어오게 함
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
