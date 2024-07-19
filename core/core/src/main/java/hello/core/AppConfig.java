8e4714fae6c2013eafbee83f38fae1e4be855b8a
taeyoung0 <jty19954@gmail.com>
2024-07-19 14:36:21 +0900

    추가- AppConfig를 스프링 컨테이너에 등록해서 사용

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -2,28 +2,34 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {        // 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다
 
+    @Bean
     public MemberService memberService() {      // 생성자 주입, 실제 동작에 필요한 구현 객체를 생성
         return new MemberServiceImpl(memberRepository()); // 생성한 객체의 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
     }
-
-    private MemberRepository memberRepository() {       // 역할에 따른 구현 클래스를 한 눈에 들어오게 함
+    @Bean
+    public MemberRepository memberRepository() {       // 역할에 따른 구현 클래스를 한 눈에 들어오게 함
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();    // 할인 정책을 변경해도 애플리케이션의 구성 역할을 담당하는 AppConfig만 변경하면됨
     }
 }
--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,21 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // 필요한 객체를 직접 사용해서 조회하는 방법
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 스프링 컨텍스트 = 스프링 컨테이너
+        // 스프링 컨테이너에 스프링 빈을 등록해서 스프링 빈을 찾아 사용하는 방법
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
 
         Member member = new Member(1L, "memberA", Grade.VIP);
