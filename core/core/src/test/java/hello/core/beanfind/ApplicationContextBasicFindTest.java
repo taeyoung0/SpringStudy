57565efa3af60b97bb56de3ed1bac65b39736155
taeyoung0 <jty19954@gmail.com>
2024-07-21 12:06:32 +0900

    추가- 빈 타입으로 조회 테스트

--- core/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -19,6 +19,12 @@ class ApplicationContextBasicFindTest {
     void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
 
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 }
