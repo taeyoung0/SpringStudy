174597732a2ec22f79b606b4ad5d4b97c0305fcb
taeyoung0 <jty19954@gmail.com>
2024-07-31 18:11:35 +0900

    추가- 롬북 적용

--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.*;
 import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor            // final이 붙은 필드를 모아서 생성자를 자동으로 만듦 (ctrl+f12로 확인 가능)
 public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -25,11 +27,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-        @Autowired          // 생성자가 1개 이면 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {     // 외부에서 의존관계 주입, DIP 지킴
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
