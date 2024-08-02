8788b0b0a2864b5138724ae9eb3dffeba6d43f4d
taeyoung0 <jty19954@gmail.com>
2024-08-02 12:08:45 +0900

    추가- 설정 정보에 초기화, 소멸 메서드 지정 initMethod, destroyMethod

--- core/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,8 +20,8 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
-        public NetworkClient networkClient() {
+        @Bean(initMethod = "init", destroyMethod = "close") // 설정 정보에 초기화, 소멸 메서드 지정, destroyMethod 는 기본값이 (inferred) (추론)으로 등록
+        public NetworkClient networkClient() {              // 이 추론 기능은 close shutdown이라는 이름의 메서드를 자동으로 호출해줌
             NetworkClient networkClient = new NetworkClient(); // 스프링 빈 라이프 사이클: 객체 생성 -> 의존 관계 주입
             networkClient.setUrl("http://hello-spring.dev");    // 객체의 생성과 초기화를 분리하자
             return networkClient;
