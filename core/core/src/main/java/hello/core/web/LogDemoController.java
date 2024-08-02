8f2776b32babfcec88d40a20af7fe7d0b5aa1cdc
taeyoung0 <jty19954@gmail.com>
2024-08-02 17:18:19 +0900

    추가- ObjectProvider로 request scope 빈의 생성을 지연시켜서 정상 동작

---
+++ core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // ObjectProvider를 사용해서 myLogger를 찾을 수있는 DL이 주입된다
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) { // HttpServletRequest 자바에서 제공하는 표준 서블릿 규약, 고객 요청 정보를 받을 수 있음
+
+        String requestURL = request.getRequestURI().toString(); // requestURL 값 http://localhost:8080/log-demo
+        MyLogger myLogger = myLoggerProvider.getObject();   //ObjectProvider.getObject() 를 호출하는 시점까지 request scope 빈의 생성을 지연
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
