0a4521b59d0ed1f62333fcf13685ea1d35359c92
taeyoung0 <jty19954@gmail.com>
2024-08-02 15:46:32 +0900

    추가- JSR330 Provider 사용

--- core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,7 @@ public class SingletonWithPrototypeTest {
 
         ClientBean prototypeBean2 = ac.getBean(ClientBean.class);
         int count2 = prototypeBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         // 싱글톤 빈은 생성 시점에만 의존 관계를 주입받기 때문에 프로토타입 빈이 새로 생성되긴 하지만 싱글톤 빈과 계속 유지되는 것(프로토타입 의미 없어짐)
         // 여러 빈에서 같은 프로토타입 빈을 주입 받으면, 주입 받은 시점에 각각 새로운 프로토타입 빈이 생성된다
     }
@@ -30,12 +31,12 @@ public class SingletonWithPrototypeTest {
     static class ClientBean {
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;    // Dependency Lookup(DL),
+        private Provider<PrototypeBean> prototypeBeanProvider;    // Dependency Lookup(DL),
         // ObjectFactory: 기능이 단순, 별도의 라이브러리 필요 없음, 스프링에 의존
         // ObjectProvider: ObjectFactory 상속, 옵션, 스트림 처리등 편의 기능이 많고, 별도의 라이브러리 필요 없음,
         //스프링에 의존
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
