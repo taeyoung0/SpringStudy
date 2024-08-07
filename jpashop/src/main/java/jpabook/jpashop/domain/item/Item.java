2b3bd7b83ea346216cfd0bdd21ffbbaf9405b38e
taeyoung0 <jty19954@gmail.com>
2024-08-07 13:56:37 +0900

    추가- 상품 엔티티 비지니스 로직, 상품 리포지토리 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스로직==// 객체지향적 설계를 위해 데이터를 가지고 있는 곳에서 비지니스 로직을 만듬, 응집도를 높이고 해당 클래스가 자율적인 객체가 된다
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.type.EntityType;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item);       // persist는 새로운 레코드를 추가
+        }
+        else {
+            em.merge(item);     // merge는 기존 레코드를 업데이트
+        }
+    }
+
+    public Item findeOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return  em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+
+    }
+}
